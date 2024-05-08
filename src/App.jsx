e83a4a2d66e78bc1973fb20e4fe53904dfa641e3
adryanbartolome <adryanbartolome8123@gmail.com>
2024-05-07 21:22:07 -0700

    Completed 'Sign Up Component' tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,12 @@
-
+import Authenticate from './components/Authenticate'
+import SignUpForm from './components/SignUpForm'
 import './App.css'
+
+export default function App() {
+  return (
+    <>
+    <Authenticate />
+    <SignUpForm />
+    </>
+  );
+}
